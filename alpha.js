cb99175fc415592cc88d38b86b2b1221f19b6136
tanvirevan <pro.tanvirevan@gmail.com>
2024-07-06 13:29:41 +0600

    Done 100%

--- alpha.js
+++ alpha.js
@@ -1,7 +1,57 @@
+
+function handleKeyboardButtonPress(event)
+    {
+        const playerPressed = event.key;
+        console.log(event.key);
+
+
+        const currentAlphabetElement = document.getElementById('current-alphabet');
+        const currentAlphabet = currentAlphabetElement.innerText;
+        const expectedAlphabet = currentAlphabet.toLowerCase();
+
+        if(playerPressed === expectedAlphabet)
+            {
+                // Update Score 
+                const currentScore = getElementTextById('current-score');
+                const newScore = currentScore + 1;
+
+                setElementTextById('current-score',newScore);
+
+
+
+                removeBackgroundColorById(expectedAlphabet);
+                continueGame();
+            }
+        else
+            {
+                // update life
+                const currentLife = getElementTextById('current-life');
+                const newLife = currentLife -1;
+                setElementTextById('current-life',newLife);
+                if(newLife === 0)
+                    {
+                        gameOver();
+                        const totalScore = getElementTextById('current-score');
+                        setElementTextById('total-score',totalScore);
+                        const hightScore = getElementTextById('hight-score');
+                        if(hightScore < totalScore)
+                            {
+                                setElementTextById('hight-score',totalScore);
+                            }
+                        else
+                            {
+                                setElementTextById('hight-score',hightScore);
+                            }
+                    }
+            }
+    }
+document.addEventListener('keyup',handleKeyboardButtonPress);
+
+
 function continueGame()
     {
         const alphabet = getARandomAlphabet();
-        console.log('Your alphabet ', alphabet);
+
 
         const currentAlphabetElement = document.getElementById('current-alphabet');
         currentAlphabetElement.innerText = alphabet;
@@ -14,5 +64,22 @@ function play()
     {
         hideElementById('home-screen');
         showElementById('play-ground');
+        continueGame();
+    }
+function gameOver()
+    {
+        hideElementById('play-ground');
+        showElementById('final-score');
+    }
+function playAgain()
+    {
+        hideElementById('final-score');
+        showElementById('play-ground');
+
+        setElementTextById('current-life', 3);
+        setElementTextById('current-score', 0);
+
+        const currentAlphabet =getElementTextById('current-alphabet');
+        removeBackgroundColorById(currentAlphabet);
         continueGame();
     }
--- utility.js
+++ utility.js
@@ -14,7 +14,24 @@ function setBackgroundColorById(elementId)
         const element = document.getElementById(elementId)
         element.classList.add('bg-orange-400');
     }
+function removeBackgroundColorById(elementId)
+    {
+        const element = document.getElementById(elementId);
+        element.classList.remove('bg-orange-400');
+    }
 
+function getElementTextById(elementId)
+    {
+        const element = document.getElementById(elementId);
+        elementValue = parseInt(element.innerText);
+        return elementValue;
+    }
+
+function setElementTextById(elementId,value)
+    {
+        const element = document.getElementById(elementId)
+        element.innerText = value;
+    }
 function getARandomAlphabet()
     {
         // get or Create an alphabet array
